5c3397e65cb2c1ca0f3c42092aba12e744edee12
Толстоногов Алексей <tolstonogovav@cifra-k.ru>
2020-07-23 17:01:00 +0700

    During task

--- src/ru/job4j/Gc.java
+++ src/ru/job4j/Gc.java
@@ -6,19 +6,11 @@ import java.util.List;
 public class Gc {
     public static void main(String[] args) {
         List<User> users1 = new ArrayList<>();
-        int countObject1 = 0;
         for (int i = 0; i < 100000; i++) {
             users1.add(new User());
-            System.out.println("to users2 added object № " + ++countObject1);
+//            if (i == (i / 100) * 100) {
+                System.out.println("to users1 added object № " + i);
+//            }
         }
-        users1 = null;
-
-        List<User> users2 = new ArrayList<>();
-        int countObject2 = 0;
-        boolean cond = true;
-        while (cond) {
-            users2.add(null);
-            System.out.println("to users2 added object № " + ++countObject2);
-        }
-    }
+     }
 }
